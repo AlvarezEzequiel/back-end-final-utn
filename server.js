ecae7377fcfcd8e43ba06a4fb48e05c6b8d787e2
Alvarez Ezequiel <alvarezezequiel00@gmail.com>
2024-07-16 17:43:05 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require ('express')
+const dotenv = require ('dotenv')
+const cors = require ('cors')
+dotenv.config()
+
+const { authRouter } = require('./auth/auth.route')
+const { productRouter } = require('./products/products.router')
+const { cartsRouter } = require('./carts/carts.router')
+
+
+
+
+
+
+const PORT = process.env.PORT || 4040
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.use('/api/auth', authRouter)
+app.use('/api/products', productRouter)
+app.use('/api/carts', cartsRouter)
+
+
+
+app.listen( PORT, () => {
+    console.log( 'Server running on port ' + PORT)
+} )
+
